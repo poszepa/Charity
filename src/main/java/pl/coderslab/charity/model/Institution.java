7939f9fdd9531a07bc384954269bbf0135bb4b34
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2022-06-24 18:00:03 +0200

    Add getters, setters , builder , contructors

--- src/main/java/pl/coderslab/charity/model/Institution.java
+++ src/main/java/pl/coderslab/charity/model/Institution.java
@@ -1,10 +1,14 @@
 package pl.coderslab.charity.model;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import java.util.List;
 
 @Entity
+@Getter @Setter @Builder
+@NoArgsConstructor @AllArgsConstructor
 public class Institution extends EntityBasic{
 
     private String name;
