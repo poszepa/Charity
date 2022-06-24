526d462806274e9bfd7e6020286c64d3e0e51575
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2022-06-24 18:00:27 +0200

    Add model to show every institution

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -1,16 +1,30 @@
 package pl.coderslab.charity;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.model.Institution;
+import pl.coderslab.charity.repository.InstitutionRepository;
+
+import java.util.List;
 
 
+@RequiredArgsConstructor
 @Controller
 public class HomeController {
 
+    private final InstitutionRepository institutionRepository;
 
     @RequestMapping("/")
     public String homeAction(Model model){
         return "index";
+    }
+
+
+    @ModelAttribute("institutions")
+    public List<Institution> institutionList() {
+        return institutionRepository.findAll();
     }
 }
