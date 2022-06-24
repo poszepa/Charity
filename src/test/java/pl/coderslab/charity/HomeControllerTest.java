6a1d2100536f1917b789740ab992c11e5dc30414
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2022-06-24 17:41:06 +0200

    Add test to home site

---
+++ src/test/java/pl/coderslab/charity/HomeControllerTest.java
@@ -0,0 +1,30 @@
+package pl.coderslab.charity;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Home Controller specification")
+@WebMvcTest(HomeController.class)
+class HomeControllerTest {
+
+
+    @Autowired private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Should prepare a view for home site")
+    public void homeControllerView_success() throws Exception {
+        mockMvc.perform
+                (MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("index"))
+                .andDo(MockMvcResultHandlers.print());
+    }
+}
