411c2e0c1652754f2391e8eb9c719323bfff86a1
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2022-06-24 20:37:06 +0200

    Created donation repository

---
+++ src/main/java/pl/coderslab/charity/repository/DonationRepository.java
@@ -0,0 +1,13 @@
+package pl.coderslab.charity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import pl.coderslab.charity.model.Donation;
+
+@Repository
+public interface DonationRepository extends JpaRepository<Donation, Long> {
+
+    @Query("SELECT SUM(donation.quantity) FROM Donation donation")
+    Integer sumQuantity();
+}
