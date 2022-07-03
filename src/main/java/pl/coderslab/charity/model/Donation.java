3de144bcabceae30a7bf77cdd336e330c482883d
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2022-07-03 13:39:40 +0200

    Added validation to every field

--- src/main/java/pl/coderslab/charity/model/Donation.java
+++ src/main/java/pl/coderslab/charity/model/Donation.java
@@ -1,11 +1,14 @@
 package pl.coderslab.charity.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
@@ -16,6 +19,8 @@ import java.util.List;
 @NoArgsConstructor @AllArgsConstructor
 public class Donation extends EntityBasic{
 
+    @Min(1)
+    @NumberFormat
     private Integer quantity;
 
     @ManyToMany
@@ -24,14 +29,26 @@ public class Donation extends EntityBasic{
     @ManyToMany
     private List<Institution> institution;
 
+    @Size(min = 2)
+    @NotNull
     private String street;
 
+    @Size(min = 2)
+    @NotNull
     private String city;
 
+    @NotNull
     private String zipCode;
 
+    @NotNull
+    @NumberFormat
+    @Min(1)
+    private Integer phoneNumber;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate pickUpDate;
 
+    @DateTimeFormat(pattern = "HH:MM")
     private LocalTime pickUpTime;
 
     private String pickUpComment;
