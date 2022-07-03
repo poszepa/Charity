ca20f19602128bfd4c9279f9f9870eb49f415ad0
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2022-07-03 13:37:01 +0200

    Add only testSerive class

---
+++ src/test/java/pl/coderslab/charity/service/DonationServiceTest.java
@@ -0,0 +1,12 @@
+package pl.coderslab.charity.service;
+
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Donation service test")
+class DonationServiceTest {
+
+
+
+}
