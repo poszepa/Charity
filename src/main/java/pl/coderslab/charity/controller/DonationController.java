d1e8e9ed153783a7bdffcd76c083629965e63a46
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2022-07-03 17:04:16 +0200

    add valid

--- src/main/java/pl/coderslab/charity/controller/DonationController.java
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -3,14 +3,18 @@ package pl.coderslab.charity.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.coderslab.charity.model.Category;
 import pl.coderslab.charity.model.Donation;
 import pl.coderslab.charity.model.Institution;
 import pl.coderslab.charity.repository.CategoryRepository;
+import pl.coderslab.charity.repository.DonationRepository;
 import pl.coderslab.charity.repository.InstitutionRepository;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -19,11 +23,21 @@ public class DonationController {
 
     private final CategoryRepository categoryRepository;
     private final InstitutionRepository institutionRepository;
+    private final DonationRepository donationRepository;
 
     @GetMapping("/donation")
     public String donationForm(Model model) {
         model.addAttribute("donation", new Donation());
         return "form";
+    }
+
+    @PostMapping("/donation")
+    public String donationFormPost(@ModelAttribute("donation")@Valid Donation donation, BindingResult bindingResult){
+        if(bindingResult.hasErrors()) {
+            return "form";
+        }
+        donationRepository.save(donation);
+        return "redirect:/";
     }
 
     @ModelAttribute("listOfCategory")
