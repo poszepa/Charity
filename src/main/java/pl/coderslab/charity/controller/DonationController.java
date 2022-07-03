fda1fe5e70f780a23fb310e7af3b3a0ee74b33a8
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2022-07-03 13:38:26 +0200

    Add view and modelAttribiute to donation

--- src/main/java/pl/coderslab/charity/controller/DonationController.java
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -1,13 +1,38 @@
 package pl.coderslab.charity.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import pl.coderslab.charity.model.Category;
+import pl.coderslab.charity.model.Donation;
+import pl.coderslab.charity.model.Institution;
+import pl.coderslab.charity.repository.CategoryRepository;
+import pl.coderslab.charity.repository.InstitutionRepository;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class DonationController {
 
+    private final CategoryRepository categoryRepository;
+    private final InstitutionRepository institutionRepository;
+
     @GetMapping("/donation")
-    public String donationForm() {
+    public String donationForm(Model model) {
+        model.addAttribute("donation", new Donation());
         return "form";
+    }
+
+    @ModelAttribute("listOfCategory")
+    public List<Category> getListCategory() {
+        return categoryRepository.findAll();
+    }
+
+    @ModelAttribute("listOfInstitution")
+    public List<Institution> getListInstitution() {
+        return institutionRepository.findAll();
     }
 }
